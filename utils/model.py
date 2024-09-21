9b92e58762ee89b6e28f76291e79da85bb9df0c8
Rajat-Dulal <rjtdulal@gmail.com>
2024-09-21 16:08:25 +0545

    First Commit

---
+++ utils/model.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+import torch
+
+class CNN(nn.Module):
+    def __init__(self, n_cls):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.flatten = nn.Flatten()
+        self.linear1 = nn.Linear(64* 32 * 32, 256)
+        self.linear2 = nn.Linear(256, n_cls)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.flatten(x)
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.linear2(x)
+        return x
